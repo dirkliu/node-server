0cd4967e49a903f6c0e6c9bb6da4a1dfe456a6d5
Liu7 <liuqi0702@126.com>
2019-03-12 19:35:55 +0800

    add assets

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
+app.use(express.static('assets'))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(90, () => console.log('Node server is listening on port 90'))
-
