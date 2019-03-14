b61baf9d7d02b049c8c37590984ba08fbfa91bfc
Liu7 <liuqi0702@126.com>
2019-03-14 19:19:58 +0800

    Add html

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function (req, res, next) {
+  console.log('users route change')
+  next()
+})
+
+router.use(function (req, res, next) {
+  console.log('users route change1')
+  next()
+})
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/detail', function(req, res, next) {
+  res.send('respond with a resource detail');
 });
 
 module.exports = router;
