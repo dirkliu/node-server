9993639c527e3929c0387ceee622d4d0b64cec2e
dirkliu <liuqi0702@126.com>
2020-04-04 16:55:06 +0800

    mock接口模拟！

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const url = require('url');
 var fs = require('fs');
+var Mock = require('mockjs')
 
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 
 var imweb = require('./imweb')
-var upload = multer({dest: 'uploads'});
+// var upload = multer({dest: 'uploads'});
+
 
 router.use(function (req, res, next) {
   req.tag__author = 'Liu7'
@@ -30,26 +32,41 @@ router.get('/detail', function(req, res, next) {
   });
 })
 
-router.post('/upload', upload.any(), function (req, res, next) {
-	var ext = path.extname(req.files[0].originalname)
-	var des_file = req.files[0].path + ext;
-	fs.readFile(req.files[0].path, function (err, data) {
-		fs.writeFile(des_file, data, function (err) {
-			if (err) {
-				console.log(err)
-			} else {
-				fs.unlink(req.files[0].path)
-				console.log('req.files[0].filename:', req.files[0].filename)
-				console.log('req.headers:', req.headers)
-				res.json({
-					msg: 'success',
-					data: {
-						path: 'http://' + req.headers.host + '/' + req.files[0].filename + ext
-					}
-				})
-			}
-		})
-	})
+router.get('/users', function (req, res, next) {
+	res.json(Mock.mock({
+		msg: 'ok',
+		code: 0,
+		'data|20': [{
+			'id|+1': 1,
+			'name|+1': [
+				Mock.Random.cname(),
+				Mock.Random.cname(),
+				Mock.Random.cname()
+			]
+		}]
+	}))
 })
+
+// router.post('/upload', upload.any(), function (req, res, next) {
+// 	var ext = path.extname(req.files[0].originalname)
+// 	var des_file = req.files[0].path + ext;
+// 	fs.readFile(req.files[0].path, function (err, data) {
+// 		fs.writeFile(des_file, data, function (err) {
+// 			if (err) {
+// 				console.log(err)
+// 			} else {
+// 				fs.unlink(req.files[0].path)
+// 				console.log('req.files[0].filename:', req.files[0].filename)
+// 				console.log('req.headers:', req.headers)
+// 				res.json({
+// 					msg: 'success',
+// 					data: {
+// 						path: 'http://' + req.headers.host + '/' + req.files[0].filename + ext
+// 					}
+// 				})
+// 			}
+// 		})
+// 	})
+// })
 
 module.exports = router
