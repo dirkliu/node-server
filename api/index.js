5c0646cc82e804be0e1fb963ee21acfe415572f2
Liu7 <liuqi0702@126.com>
2020-04-07 19:01:22 +0800

    删除uploads

--- api/index.js
+++ api/index.js
@@ -47,26 +47,4 @@ router.get('/users', function (req, res, next) {
 	}))
 })
 
-// router.post('/upload', upload.any(), function (req, res, next) {
-// 	var ext = path.extname(req.files[0].originalname)
-// 	var des_file = req.files[0].path + ext;
-// 	fs.readFile(req.files[0].path, function (err, data) {
-// 		fs.writeFile(des_file, data, function (err) {
-// 			if (err) {
-// 				console.log(err)
-// 			} else {
-// 				fs.unlink(req.files[0].path)
-// 				console.log('req.files[0].filename:', req.files[0].filename)
-// 				console.log('req.headers:', req.headers)
-// 				res.json({
-// 					msg: 'success',
-// 					data: {
-// 						path: 'http://' + req.headers.host + '/' + req.files[0].filename + ext
-// 					}
-// 				})
-// 			}
-// 		})
-// 	})
-// })
-
 module.exports = router
